09cc64a74fe77c3e0b1117aad03f21363fde0cf4
unknown <blackghost2693@gmail.com>
2024-09-12 10:57:46 +0900

    Postgres connect in chapter 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <NavLinks />
+      {/* <NavLinks /> */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
