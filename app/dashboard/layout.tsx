83e6d82b675eb786b0eac9d6d91a98e37ff893cd
unknown <blackghost2693@gmail.com>
2024-09-13 10:58:40 +0900

    Chapter 9 completed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+export const experimental_ppr = true;
 export default function Layout({ children }:{children:React.ReactNode}) {
   return (
     <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
