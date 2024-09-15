3e5a774b63827c228e1a9588e20ef2565a0577e5
unknown <blackghost2693@gmail.com>
2024-09-16 03:53:53 +0900

    Chapter 10 completed

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,11 +4,14 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname,useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
-
-  // const allPages = generatePagination(currentPage, totalPages);
+  const searchParams = useSearchParams();
+  const pathname=usePathname(); 
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
