e48f0d605d64993d82262bd3700b1dacbf8d467c
unknown <blackghost2693@gmail.com>
2024-09-16 09:32:59 +0900

    Chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
  
 export default async function Page({params}: {params: {id: string}}) {
   const id = params.id;
