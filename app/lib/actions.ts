e0757903690d42ecc15065306da0822e409566e7
unknown <blackghost2693@gmail.com>
2024-09-30 10:25:39 +0900

    Chapter 15 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,27 +7,46 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
 
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    console.log("--",customerId, amount, status);
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Please fill in all required fields',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
     try {
-    await sql`INSERT INTO invoices (customer_id, amount, date, status) 
+        await sql`INSERT INTO invoices (customer_id, amount, date, status) 
   VALUES (${customerId}, ${amountInCents}, ${status}), ${date}`;
     }
     catch (error) {
@@ -70,7 +89,7 @@ export async function deleteInvoice(id: string) {
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
-    return { message: 'Invoice deleted successfully' };
+        return { message: 'Invoice deleted successfully' };
     }
     catch (error) {
         return { message: 'database Error: Failed to delete invoice' };
